6c425e5af41feaff828b23a7f93d3ecf3f1071bf
Muhammad Saqib <saqibinzergi@gmail.com>
2026-09-04 03:24:46 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+// Create web server
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello, world!");
+});
+
+server.listen(3000);
